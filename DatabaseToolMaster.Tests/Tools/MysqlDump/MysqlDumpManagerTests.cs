2f378614f5231f53434b88be7259c367cab7bd55
scherenhaenden <scherenhaenden@gmail.com>
2023-05-09 13:35:19 +0200

    update tests

--- DatabaseToolMaster.Tests/Tools/MysqlDump/MysqlDumpManagerTests.cs
+++ DatabaseToolMaster.Tests/Tools/MysqlDump/MysqlDumpManagerTests.cs
@@ -27,9 +27,38 @@ public class MysqlDumpManagerTests
         var processCreator = new ProcessCreator();
         
         // Arrange
-        var mysqlDumpManager = new MysqlDumpManager(processCreator);
+        var mysqlDumpManager = new MysqlDumpManager();
         
         await mysqlDumpManager.LoadBackupToDatabase(dbHost, dbName, dbUser, dbPass, _backup);
+    }
+    
+    [Test]
+    public async Task CloneDump()
+    {
+        var dbName = "test";
+        var dbUser = "root";
+        var dbPass = "test";
+        var dbHost = "localhost";
+        
+        
+
+        // Create process
+        
+        
+        // Arrange
+        ;
+        
+        
+        _backup = new MysqlDumpManager().GetRawOfDump(dbName, dbUser, dbPass, dbHost);
+        await new MysqlDumpManager().LoadBackupToDatabaseCloning(dbHost, dbName, "test1",dbUser, dbPass, _backup);
+        
+        
+        // create a loop to create 100 databases
+        for(int i = 0; i < 500; i++)
+        {
+             await new MysqlDumpManager().LoadBackupToDatabaseCloning(dbHost, dbName, "test1" + (i+600),dbUser, dbPass, _backup);
+        }
+
     }
     
    
